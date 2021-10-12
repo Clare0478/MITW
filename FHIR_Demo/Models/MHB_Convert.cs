6bbc4cfbbcef7e47112adfe3c73bb6cbdd359f27
HsinHua <hsinhuakung@gmail.com>
2021-10-12 10:04:15 +0800

    加入專案檔案。

---
+++ FHIR_Demo/Models/MHB_Convert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Xml;
+
+namespace FHIR_Demo.Models
+{
+    public class MHB_Convert
+    {
+
+
+    }
+}
